850461e7d1c85fd90261a3bb3aa92e50b105c5b0
Anggisandra <anggisandra811@gmail.com>
2023-08-30 20:13:42 +0700

    Belajar

---
+++ belajar/pertemuan1.py
@@ -0,0 +1,40 @@
+pergi ke ATM 
+Masukkan kartu
+masukkan pin
+jika pin benar : 
+    masuk menu : 
+        1. penarikan uang 
+        2. tambah saldo 
+        3. cek saldo
+        4. keluar ATM
+        Jika memilih menu 1 : 
+            uang = saldo - jumlah uang yang ingin ditarik
+        jika memilih menu 2 :
+            uang - saldo + jumlah yang di tambah
+        jika memilih menu 3 :
+            saldo = saldo 
+        jika memilih menu 4 :
+            keluar ATM
+jika pin salah :
+    masukkan pin ulang sampai benar
+jika salah 3x : ATM akan terblokir 
+
+bangun tidur 
+sholat subuh
+menyapu rumah 
+mnyusun perabotan rumah 
+mandi
+melipat pakaian 
+menggangu alana magang 
+mencari pekerjaan 
+mengganggu wak ibuk memasak 
+makan siang 
+sholat dzuhur
+tidur siang
+DS magang eduwork 
+membuat laporan DS eduwork 
+sholat ashar
+mencari pekerjaan 
+menyusun perabotan 
+tidur malam
+
---
+++ belajar/pertemuan2.py
@@ -0,0 +1,40 @@
+#String
+anggi = 'anggi'
+
+#Ineteger
+angka = 1233438734763649
+
+#Float
+angka1 = 3.123
+
+#Boolean
+boolean = True
+boolear = False
+
+print(6 < 3)
+
+a = 5
+b = 3
+c = a + b
+
+print(c)
+
+#array
+array = [1, 2, 3, 4, 5]
+array2 = ["anggi", "ayang", "alana"]
+print(array2)
+
+print(array)
+
+anggiSandra = "anggi"
+alanut = "lanut"
+# print(anggiSandra + alanut)
+
+angka = 28
+nomor = 10.13
+print(angka + nomor)
+
+
+for i in range (5):
+    print(alanut)
+
